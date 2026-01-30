53196f6766a775813c2eb7c1a3e2ad68f109d282
abraham <abrahamkifle1212@gmail.com>
2026-01-30 22:33:02 +0300

    feat: add Navbar component with a logo and a logout button.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { assets } from '../assets/admin_assets/assets'
 const Navbar = () => {
     return (
         <div>
-            <img src={assets.logo} alt="" />
-            <button>Logout</button>
+            <img className='w-[max(10%, 80px)]' src={assets.logo} alt="" />
+            <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full  text-sx sm:text-sm '>Logout</button>
         </div>
     )
 }
